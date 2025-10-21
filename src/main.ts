08ad125e0a033c6cecf669e1cedf8df9ae86de9c
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2025-10-21 15:09:15 +0300

    fix cors3

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,26 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://incode-front.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void
+    ) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://incode-front.vercel.app'
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true
   });
 
   app.useGlobalPipes(new ValidationPipe());
