1797556ca64c6cf7c706839cd730c563cd41d09d
Denis-isnotavailable <92397311+Denis-isnotavailable@users.noreply.github.com>
2025-10-21 14:10:34 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://incode-front.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
